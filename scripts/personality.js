ee2768e9ce8f5554840bb206014e1ae29d0272bb
Brian Mitchell <bman4789@gmail.com>
2018-05-26 20:50:59 -0500

    Add some fun to the greetings

--- scripts/personality.js
+++ scripts/personality.js
@@ -2,7 +2,10 @@
 //   hi|hello|hey|sup - Greetings!
 
 module.exports = robot => {
-  robot.hear(/hi|hello|hey|sup/i, res => {
-    res.send('Sup\' yo.');
+  robot.hear(/(hi|hello|heyo?|sup)(?:\s|$)/i, res => {
+    const greeting = res.match[1].charAt(0).toUpperCase() + res.match[1].slice(1);
+    if (Math.random() > 0.97) {
+      res.send(`${greeting} ${res.message.user.real_name}! :smile: Nice to see you!`);
+    }
   });
 };
