9a76c418f85b7b9baeb04cd9e6ab3274dcf26cee
Brian Mitchell <bman4789@gmail.com>
2018-05-31 21:26:58 -0500

    Improve regex and lower response threshold (fixes #3)

--- scripts/personality.js
+++ scripts/personality.js
@@ -2,9 +2,9 @@
 //   hi|hello|hey|sup - Greetings!
 
 module.exports = robot => {
-  robot.hear(/(hi|hello|heyo?|sup)(?:\s|$)/i, res => {
+  robot.hear(/(?:^|\s)(hi|hello|heyo?|sup)(?:\s|$)/i, res => {
     const greeting = res.match[1].charAt(0).toUpperCase() + res.match[1].slice(1);
-    if (Math.random() > 0.97) {
+    if (Math.random() >= 0.9) {
       res.send(`${greeting} ${res.message.user.real_name}! :smile: Nice to see you!`);
     }
   });
